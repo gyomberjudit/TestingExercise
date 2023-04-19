16acd705e7b255fa27ae0feca9264e237b62c8df
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-19 15:18:24 +0200

    add BasePage

--- src/main/java/org/example/BasePage.java
+++ src/main/java/org/example/BasePage.java
@@ -7,15 +7,11 @@ import org.openqa.selenium.WebElement;
 
 public class BasePage {
     protected final WebDriver driver;
-    private final String URL = "https://demo.seleniumeasy.com/";
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
     }
-
-    public void navigate() {
-        driver.navigate().to(URL);
-    }
+    
     public void scrollToElement(By xpath) throws InterruptedException
     {
         JavascriptExecutor js = (JavascriptExecutor) driver;
