14b3938fa6d34e9c78737c2fc6591e91883020ea
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-19 19:02:18 +0200

    refresh BasePage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,9 +1,6 @@
-package org.example;
+package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class BasePage {
     protected final WebDriver driver;
@@ -11,12 +8,12 @@ public class BasePage {
     public BasePage(WebDriver driver) {
         this.driver = driver;
     }
-    
-    public void scrollToElement(By xpath) throws InterruptedException
+
+   /* public void scrollToElement(By xpath) throws InterruptedException
     {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         WebElement element = driver.findElement(xpath);
         js.executeScript("arguments[0].scrollIntoView(true);", element);
         Thread.sleep(500);
-    }
+    }*/
 }
