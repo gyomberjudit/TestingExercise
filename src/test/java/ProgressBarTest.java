80bf8f6749e77d41a4db4f8f7ec9b7f77c8cd440
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-19 15:20:06 +0200

    add ProgessBarTest.java

--- src/test/java/ProgressBarTest.java
+++ src/test/java/ProgressBarTest.java
@@ -1,2 +1,22 @@
+import org.example.ProgressBarPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
 public class ProgressBarTest extends BaseTest{
+
+    ProgressBarPage progressBarPage;
+    @Test
+    public void TestProgressBarDownload()
+    {
+        progressBarPage = new ProgressBarPage(driver);
+        progressBarPage.navigate();
+
+        progressBarPage.clickDownload();
+
+        Assertions.assertTimeout(Duration.ofSeconds(100), () -> {
+            progressBarPage.readPercentage();
+        });
+    }
 }
