70f194ff9472cfcdfa78e9446c445e80c4338b8d
Gyömbér Judit <gyomberjudit@gmail.com>
2023-04-19 21:13:38 +0200

    add BasePage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,22 +1,13 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class BasePage {
-    protected final WebDriver driver;
+
+    WebDriver driver;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void scrollToElement(By xpath) throws InterruptedException
-    {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement element = driver.findElement(xpath);
-        js.executeScript("arguments[0].scrollIntoView(true);", element);
-        Thread.sleep(500);
-    }
 }
